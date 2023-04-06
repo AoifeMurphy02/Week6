5a3d1fe95f4a8d5b027c13d433b831aed758d622
aoife <aoife@192.168.1.197>
2023-04-06 18:50:36 +0100

    Add method to the queue with priority

--- Week6/src/main/java/week6/task/BoundedPriorityQueueSet.java
+++ Week6/src/main/java/week6/task/BoundedPriorityQueueSet.java
@@ -19,7 +19,44 @@ public class BoundedPriorityQueueSet extends LinkedList {
         super();
         max = MAX;
     }
+    @Override
+ public boolean add(Task value) {
 
+        Node newNode = new Node(value);
+
+        if (isEmpty()) {
+
+            first = newNode;
+            last = newNode;
+        }
+ 
+        else if(value.compareTo( first.data) <0){
+         newNode.next = first;
+          first = newNode;
+         
+        }
+         else{
+           Node current = first.next;
+            Node prev = first;
+            while(current != null && value.compareTo(current.data)>0){
+                prev = current;
+                current = current.next;
+        }
+         
+        
+            prev.next = newNode;
+            newNode.next = current;
+        
+        if(current == null){
+          last = newNode;  
+        }
+        }
+        return true;
+    }
+
+
+
+    
     public BoundedPriorityQueueSet(int max) {
         max = MAX;
         this.max = max;
