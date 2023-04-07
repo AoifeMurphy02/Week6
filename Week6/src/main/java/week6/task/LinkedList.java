f6ae48cc69f74f4260a9537c2af5aafb04d66331
aoife <aoife@192.168.1.197>
2023-04-07 21:05:32 +0100

    Added Java Docs

--- Week6/src/main/java/week6/task/LinkedList.java
+++ Week6/src/main/java/week6/task/LinkedList.java
@@ -14,27 +14,49 @@ public class LinkedList {
     protected Node last;
     protected int size;
 
+    /**
+     * Constructs an empty list
+     */
     public LinkedList() {
         first = null;
         last = null;
         size = 0;
     }
 
+    /**
+     * removes all Nodes from the list
+     */
     public void clear() {
         first = null;
         last = null;
         size = 0;
     }
 
+    /**
+     * To see if the list is empty
+     *
+     * @return true if its empty or false if its not
+     */
     public boolean isEmpty() {
         return first == null;
         // return size == 0;
     }
 
+    /**
+     * gets the size of the list
+     *
+     * @return the size of the list
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * adds a new node containing a Task to the start of the list
+     *
+     * @param task the Task to be added
+     * @return true if tasks was added
+     */
     public boolean addFirst(Task task) {
         Node newNode = new Node(task);
         if (first == null) {
@@ -48,6 +70,14 @@ public class LinkedList {
         return true;
     }
 
+    /**
+     * Gets the Task at with the index in the list.
+     *
+     * @param index the index of the the Task to be found
+     * @return the Task at the index
+     * @throws IndexOutOfBoundsException if the index is out of range (index < 0 || index
+     * >= size).
+     */
     public Task get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Invalid position provided");
@@ -58,7 +88,14 @@ public class LinkedList {
         }
         return current.data;
     }
- public int add(Task value) throws DuplicateElementException {
+
+    /**
+     * Add a new node with the Task at the end of the list 
+     * @param value to be added to the list 
+     * @return the index where the Task was added 
+     * @throws DuplicateElementException if the Task is already in the list
+     */
+    public int add(Task value) throws DuplicateElementException {
         Node newNode = new Node(value);
         if (first == null) {
             first = newNode;
@@ -68,13 +105,16 @@ public class LinkedList {
             last = newNode;
         }
         size++;
-       int index = indexOf(value);
-       return index;
+        int index = indexOf(value);
+        return index;
     }
-
-   
-    
-
+    /**
+     * Add a new node with a Task at a index
+     * @param value the Task to be added 
+     * @param index where the Task should be added
+     * @return the index where the Task was added 
+     * @throws  IndexOutOfBoundsException if the index is out of range.
+     */
     public int add(Task value, int index) {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Invalid position provided");
@@ -82,22 +122,25 @@ public class LinkedList {
 
         if (index == 0) {
             addFirst(value);
-        }
-        else{
+        } else {
 
-        Node current = first;
-        for (int i = 0; i < index - 1; i++) {
-            current = current.next;
+            Node current = first;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            Node newNode = new Node(value);
+            newNode.next = current.next;
+            current.next = newNode;
+            size++;
+
         }
-        Node newNode = new Node(value);
-        newNode.next = current.next;
-        current.next = newNode;
-        size++;
-        
-    }
         return index;
     }
-
+    /**
+     * finds the index of the Task 
+     * @param value the Task we are looking for
+     * @return the index of the task we are looking for or -1 if its not in the list
+     */
     public int indexOf(Task value) {
         if (first == null) {
             return -1;
@@ -105,14 +148,20 @@ public class LinkedList {
 
         Node current = first;
         for (int i = 0; i < size; i++) {
-            if (value.equals( current.data)){
+            if (value.equals(current.data)) {
                 return i;
             }
             current = current.next;
         }
         return -1;
     }
-
+    /**
+     * Replaces the Task at the index in the list with a Task passed
+     * @param index the index of the Task object to be replaced 
+     * @param value the Task to be put in the index
+     * @return the Task which was at the index previously
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
     public Task set(int index, Task value) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Invalid position provided");
@@ -125,7 +174,12 @@ public class LinkedList {
         current.data = value;
         return original;
     }
-
+    /**
+     * Removed the Task at the index provided 
+     * @param index the index of the task to de removed
+     * @return the task that was removed from the list
+     * @throws IndexOutOfBoundsException if the index is out of range 
+     */
     public Task remove(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Illegal position supplied!");
@@ -139,8 +193,8 @@ public class LinkedList {
             for (int i = 0; i < index - 1; i++) {
                 current = current.next;
             }
-            
-            if(current.next == last){
+
+            if (current.next == last) {
                 last = current;
             }
             original = current.next.data;
@@ -160,5 +214,4 @@ public class LinkedList {
             this.next = null;
         }
     }
-}  
-
+}
