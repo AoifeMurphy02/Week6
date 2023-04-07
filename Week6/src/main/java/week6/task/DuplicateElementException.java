bcd33232da757051b11bfe030c1f1e0bb81a522f
aoife <aoife@192.168.1.197>
2023-04-07 21:10:30 +0100

    DuplicateElementException

---
+++ Week6/src/main/java/week6/task/DuplicateElementException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week6.task;
+
+/**
+ *
+ * @author aoife
+ */
+public class DuplicateElementException extends Exception {
+
+    public DuplicateElementException(String message) {
+        System.out.println((message));
+    }
+    
+}
+
+
